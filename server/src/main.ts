23469d7a392abe0ef86efe3c55924c543265091e
Maxim Kiselev <spl33t@ya.ru>
2023-10-27 18:17:50 +0300

    added globalPipes, globalFilters

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,43 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { UsersController } from "./users/users.controller";
 
 const PORT = 8000
+
+
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const statusCode = exception.getStatus();
+    const data = exception.getResponse();
+
+
+    response
+      .status(statusCode)
+      .json({
+        statusCode,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        data
+      });
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ credentials: true, origin: ["https://127.0.0.1:3000"] })
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+  }));
+
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(PORT, () => console.log(`NEST APP STARTED ON ${PORT} PORT`));
 }
 
