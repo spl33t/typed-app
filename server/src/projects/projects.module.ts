f59f05484f12b3c1b806963d5e96fc9d88e92492
Maxim Kiselev <spl33t@ya.ru>
2023-11-05 11:51:58 +0300

    minor changes

--- server/src/projects/projects.module.ts
+++ server/src/projects/projects.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { ProjectsModel } from "./projects.model";
 
 @Module({
-  imports: [SequelizeModule.forFeature([ProjectsModel])],
   providers: [ProjectsService],
   controllers: [ProjectsController]
 })
